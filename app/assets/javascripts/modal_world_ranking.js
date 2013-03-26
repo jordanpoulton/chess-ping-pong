cb4c6af31a851011cdb6c25328083e855f95c891
jordanpoulton <jordan@oxfordentrepreneurs.co.uk>
2013-03-26 16:43:28 +0000

    World Rankings almost done

--- app/assets/javascripts/modal_world_ranking.js
+++ app/assets/javascripts/modal_world_ranking.js
@@ -1,6 +1,8 @@
 $(function() {
   $("#world-rankings").click(function() {
     $( "#dialog-message" ).dialog({
+      width: 800,
+      height: 600,
       modal: true,
       buttons: {
         Ok: function() {
